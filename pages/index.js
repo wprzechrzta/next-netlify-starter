11186fa271c3a5a1a3778fd96fad2a3720e09f2a
wprzechrzta <w.przechrzta@gmail.com>
2023-03-15 16:59:38 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>
+          Deploy number 2
+        </p>
       </main>
 
       <Footer />
